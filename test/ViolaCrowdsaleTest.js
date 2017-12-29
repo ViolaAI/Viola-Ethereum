136312f2b8cb33f7e4503807da3683a2ffe2d51a
Daud Abas <daud.abas@2359media.com>
2017-12-29 10:18:32 +0800

    Move test setup to beforeEach hook

--- test/ViolaCrowdsaleTest.js
+++ test/ViolaCrowdsaleTest.js
@@ -1,92 +1,50 @@
+
+
 // Contract to be tested
 var ViolaCrowdSale = artifacts.require("./ViolaCrowdsale.sol")
 var ViolaToken = artifacts.require("./ViolaToken.sol")
 
 // Test suite
 contract('ViolaCrowdsale', function(accounts) {
-  var violaCrowdSaleInstance;
+  
   var violaTokenInstance
   
   // Check initial values
   describe('initialize', function() {
-    var endTime
-    it('should be initialized with a start time', function() {
-        return ViolaCrowdSale.deployed().then(function(instance) {
-          return instance.startTime.call()
-        }).then(function(startTime) {
-            assert.isAbove(startTime, 0, 'start time should not be 0')
-        })
-      })
+    var violaCrowdSaleInstance;
 
-      it('should be initialized with an end time', function() {
+    beforeEach(function() {
         return ViolaCrowdSale.deployed().then(function(instance) {
-          return instance.endTime.call()
-        }).then(function(endTime) {
-            assert.isAbove(endTime, 0, 'end time should not be 0')
+            violaCrowdSaleInstance = instance
         })
-      })
+    })
 
-      it('end time should not be lesser than start time', function() {
-        return ViolaCrowdSale.deployed().then(function(instance) {
-          violaCrowdSaleInstance = instance
-          return violaCrowdSaleInstance.endTime.call()
-        }).then(function(time) {
+    it('should have a start time', function() {
+        return violaCrowdSaleInstance.startTime.call().then(function(startTime) {
+            assert.isAbove(parseInt(startTime), 0, 'start time should not be 0')
+        })
+    })
+
+    it('should have an end time', function() {
+        return violaCrowdSaleInstance.endTime.call().then(function(endTime) {
+            assert.isAbove(parseInt(endTime), 0, 'end time should not be 0')
+        })
+    })
+
+    it('should have a rate', function() {
+        return violaCrowdSaleInstance.rate.call().then(function(rate) {
+            assert.isAbove(parseFloat(rate), 0, 'rate should not be 0')
+        })
+    })
+
+    it('end time should not be earlier than start time', function() {
+        var endTime
+        return violaCrowdSaleInstance.endTime.call().then(function(time) {
             endTime = time
             return violaCrowdSaleInstance.startTime.call()
         }).then(function(startTime) {
-            assert.isAbove(endTime, startTime, 'end time should be later than start time')
+            assert.isAbove(parseInt(endTime), parseInt(startTime), 'end time is earlier than start time')
         })
-      })
-
-      it('should be initiated with a rate', function(){
-        return ViolaCrowdSale.deployed().then(function(instance) {
-            violaCrowdSaleInstance = instance
-            return violaCrowdSaleInstance.rate.call()
-          }).then(function(rate) {
-              assert.isAbove(rate, 0, 'rate should not be 0')
-          })
-        })
-  })
-  
-
-  describe('token address', function() {
-    it('should be able to set the token address', function() {
-        return ViolaToken.deployed().then(function(instance) {
-            violaTokenInstance = instance
-            return ViolaCrowdSale.deployed()
-          }).then(function(instance) {
-              violaCrowdSaleInstance = instance
-              return instance.setToken(violaTokenInstance.address)
-          }).then(function() {
-              return violaCrowdSaleInstance.myToken.call()
-          }).then(function(data) {
-              assert.equal(data, violaTokenInstance.address, 'token address is not equal')
-          })
-    })
-
-    it('token address should not be 0', function() {
-        return ViolaCrowdSale.deployed().then(function(instance) {
-              violaCrowdSaleInstance = instance
-              assert.throws(function() {
-                  instance.setToken(0)
-              }, Error, 'Error thrown')
-          })
     })
   })
-
-
-  
-
-  
-
-
-
-  it("should be able to start crowd sale", function() {
-      return ViolaToken.deployed().then(function(instance) {
-        violaTokenInstance = instance
-      }).then(function(instance) {
-
-      })
-  })
-
-});
+})
