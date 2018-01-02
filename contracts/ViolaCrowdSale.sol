de47aeb30d5928bf49e73e262ba4745ff6b7d1c5
esmund <esmund.koh@2359media.com>
2018-01-02 15:50:23 +0800

    Edit class name and added more get functions

--- contracts/ViolaCrowdSale.sol
+++ contracts/ViolaCrowdSale.sol
@@ -12,7 +12,7 @@ import '../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol';
  * on a token per ETH rate. Funds collected are forwarded to a wallet
  * as they arrive.
  */
-contract ViolaCrowdSale is Ownable {
+contract ViolaCrowdsale is Ownable {
   using SafeMath for uint256;
 
   enum State { Preparing, NotStarted, Active, Refunding, Ended, Stopped }
@@ -36,7 +36,7 @@ contract ViolaCrowdSale is Ownable {
   // address where funds are collected
   address public wallet;
 
-  // how many token units a buyer gets per wei
+  // how many token units a buyer gets per eth
   uint256 public rate;
 
   uint public bonusTokenRate;
@@ -59,7 +59,7 @@ contract ViolaCrowdSale is Ownable {
   event TokenPurchase(address indexed purchaser, uint256 value, uint256 amount, uint256 bonusAmount);
 
 
-  function ViolaCrowdSale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _bonusRate, address _wallet) public {
+  function ViolaCrowdale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _bonusRate, address _wallet) public {
     require(_startTime >= now);
     require(_endTime >= _startTime);
     require(_rate > 0);
@@ -107,7 +107,7 @@ contract ViolaCrowdSale is Ownable {
 
   // low level token purchase function
   function buyTokens(address investor) public payable {
-    require(tx.gasprice <= 50000000000 wei);
+    //require(tx.gasprice <= 50000000 wei);
     
     require(myToken != address(0));
     require(validPurchase());
@@ -170,8 +170,21 @@ contract ViolaCrowdSale is Ownable {
   function getTokensLeft() public constant returns (uint) {
     return myToken.allowance(owner, this).sub(totalTokensAllocated);
   }
+
   function assignTokens(address receiver, uint tokenAmount) internal {
      require(myToken.transferFrom(owner, receiver, tokenAmount));
+  }
+
+  function getAddressAllocatedTokens(address investor) public constant returns(uint) {
+    return tokensAllocated[investor];
+  }
+
+  function getAddressBonusAllocatedTokens(address investor) public constant returns(uint) {
+    return bonusTokensAllocated[investor];
+  }
+
+  function getAddressAmtInvested(address investor) public constant returns(uint) {
+    return investedSum[investor];
   }
 
   function emergencyERC20Drain( ERC20 token, uint amount ) external onlyOwner {
