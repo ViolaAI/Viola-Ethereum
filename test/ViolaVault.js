16321258e236f411d65d132ba7adef0b7de72da2
Daud Abas <daud.abas@2359media.com>
2018-01-03 10:08:16 +0800

    Add ViolaVault

---
+++ test/ViolaVault.js
@@ -0,0 +1,13 @@
+// Contract to be tested
+const ViolaVault = artifacts.require('ViolaVault')
+
+const BigNumber = web3.BigNumber
+
+const should = require('chai')
+.use(require('chai-as-promised'))
+.use(require('chai-bignumber')(BigNumber))
+.should()
+
+contract('ViolaVault', function (accounts) {
+
+})
