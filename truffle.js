85f9094285d0033fbb7473913d19c68a48dd434f
Martti Dumangeng <martti@lunchactually.com>
2018-04-26 12:24:24 +0800

    added config file

--- truffle.js
+++ truffle.js
@@ -1,8 +1,11 @@
+var config = require('./config');
+
 var PrivateKeyProvider = require("truffle-privatekey-provider");
-var privateKey = "b9b927a489cd61c6d5ca2bea1578529f362f91e942f717eb31652f56329d7e90"; // test wallet
-var infura_apikey = "pY06PzI33RGJ7FVNoaCP";
+var privateKey = config.privatekey;
+var provider_url = config.provider_url + config.infura_apikey;
 
 require('babel-register')
+
 module.exports = {
   networks: {
     development: {
@@ -11,7 +14,7 @@ module.exports = {
       network_id: "1"
     },
     rinkeby: {
-      provider: new PrivateKeyProvider(privateKey, "https://rinkeby.infura.io/"+infura_apikey),
+      provider: new PrivateKeyProvider(privateKey, provider_url),
       network_id: "2"
     }
   }
